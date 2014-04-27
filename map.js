5191e5876c88925471ac1dfe87791dcad4811cd4
gizmoabhinav <gizmoabhinav@gmail.com>
2014-04-27 08:39:02 +0530

    added timeline
    [#70104698]

--- map.js
+++ map.js
@@ -1,13 +1,121 @@
+//load map
+var speed = 1;
 var map = L.map('map').setView([30, -0], 2);
-	var track = new L.KML("plates.kml", {async: true});
-   // track.on("loaded", function(e) { map.fitBounds(e.target.getBounds()); });
+map.invalidateSize(false);
+// create timeline
+var tl = new TimelineLite({onUpdate:updateSlider});
+$("#slider").slider({
+		value: 0,
+		range: "min",
+		min: 0,
+		max: 315670898300,
+		slide: function ( event, ui ) {
+			$("#values").html(timeConverter(ui.value+1072921310700));
+			//tl.seek();
+			tl.pause();
+			tl.progress(ui.value/315670898300);
+		}
+	});
+tl.pause();
+function updateSlider()
+	{
+		$("#slider").slider("value", (tl.progress()*315670898300));
+		$("#values").html(timeConverter((tl.progress()*315670898300)+1072921310700));
+	}
+//time
+function timeConverter(UNIX_timestamp){
+ var a = new Date(UNIX_timestamp);
+ var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+     var year = a.getFullYear();
+     var month = months[a.getMonth()];
+     var date = a.getDate();
+     var hour = a.getHours();
+     var min = a.getMinutes();
+     var sec = a.getSeconds();
+     var time = year+' '+month+' '+date+'  '+hour+':'+min+':'+sec ;
+     return time;
+ }
+// load data from usgs
+var size = 0;
+var circles = new Array();
+var time = new Array();
+var stamp = new Array();
+var script = document.createElement('script');
+var snd = new Audio("tap.wav"); // buffers automatically when created
+
+	script.src = 'values.js';
+	document.getElementsByTagName('body')[0].appendChild(script);
+	window.eqfeed_callback = function(results) {
+	  
+		size = results.features.length;
+			for (var i = 0; i < 19774; i++){
+				circles[i] = L.geoJson(results.features[i], {pointToLayer: function (feature, latlng) {return L.circleMarker(latlng, {radius: results.features[i].properties.mag ,fillColor: "#ff0000",color: "#000",weight: 1,opacity: 1,fillOpacity: 0.8});}}).bindPopup("Place: <b>"+results.features[i].properties.place+"</b></br>Magnitude : <b>"+ results.features[i].properties.mag+"</b>");
+				time[i] = timeConverter(results.features[i].properties.time);
+				stamp[i] = results.features[i].properties.time
+				// Adding events to the timeline
+				//if(i==0)
+				//tl.append(TweenLite.delayedCall(0,mapAdder,[i.toString]));
+				if(i>0)
+				tl.append(TweenLite.delayedCall(20*((results.features[i].properties.time-results.features[i-1].properties.time)/1000000000), mapAdder, [i.toString()]));
+				
+				//depths[i] = results.features[i].geometry.coordinates[2];
+			}
+			
+			
+			
+	}
+	function mapAdder(i){
+		if(!map.hasLayer(circles[i]))
+		map.addLayer(circles[i]);
+		if(i>=20)
+		mapRemover(i-20);
+		
+		$("#time").html(time[i]);
+		snd.play();
+		/*for(var j= Math.floor(tl.progress()*100)+1;j<19774;j++){
+				if(map.hasLayer(circles[j])){
+					map.removeLayer(circles[j]);
+				}
+			}*/
+	}
+	function mapRemover(i){
+		if(map.hasLayer(circles[i]))
+		map.removeLayer(circles[i]);
+	}
+// load plate boundaries
+var track = new L.KML("plates.kml", {async: true});
+
+
+///////////// Controls /////////////////
+
+//plate controls   
    $('#plates').click(function () {
    if($("#plates").is(':checked'))
     map.addLayer(track);  // checked
 	else
     map.removeLayer(track);  // unchecked
     });
-	map.invalidateSize(false);
+//buttons
+	function Play(){
+		tl.resume();
+	}
+	function Pause(){
+		tl.pause();
+	}
+	function SpeedUp(){
+			speed*=1.05;
+			tl.timeScale(speed);
+	}
+	function SpeedDown(){
+		if(speed>=0.5)
+			speed/=2;
+			tl.timeScale(speed);
+	}
+
+//////////// Controls end //////////////
+
+/////////////// Layers ////////////////
+
 	var osmmapLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 			maxZoom: 18,
 			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
@@ -22,11 +130,6 @@ var map = L.map('map').setView([30, -0], 2);
 		attribution: 'Map &copy; Pacific Rim Coordination Center (PRCC).  Certain data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
 	});
 	prccEarthquakesLayer.addTo(map);
-
-	// Initialize the legend control and add it to the map
-	//var legendControl = new L.Control.Legend();
-
-	//legendControl.addTo(map);
 
 	var layerControl = new L.Control.Layers({
 		'MapQuest Satellite': baseLayer,
