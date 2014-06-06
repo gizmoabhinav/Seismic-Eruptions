29b210de8b07b52e1bf64c6715742488b4775a6a
gizmoabhinav <gizmoabhinav@gmail.com>
2014-06-06 10:28:39 +0530

    particle system removed

--- 3D/3Dplot.js
+++ 3D/3Dplot.js
@@ -12,12 +12,6 @@ var script = document.createElement('script');
 script.src = 'http://comcat.cr.usgs.gov/fdsnws/event/1/query?starttime='+year+'-'+month+'-'+date+'%2000:00:00&minmagnitude='+magnitude+'&format=geojson&callback=eqfeed_callback&endtime='+curr_year+'-'+curr_month+'-'+curr_date+'%2023:59:59&orderby=time-asc&minlatitude='+Math.min(getURLParameter("y1"),getURLParameter("y2"),getURLParameter("y3"),getURLParameter("y4"))+'&maxlatitude='+Math.max(getURLParameter("y1"),getURLParameter("y2"),getURLParameter("y3"),getURLParameter("y4"))+'&minlongitude='+Math.min(getURLParameter("x1"),getURLParameter("x2"),getURLParameter("x3"),getURLParameter("x4"))+'&maxlongitude='+Math.max(getURLParameter("x1"),getURLParameter("x2"),getURLParameter("x3"),getURLParameter("x4"));
 document.getElementsByTagName('body')[0].appendChild(script);
 var count = 0;
-
-
-var particle_system_geometry = new THREE.Geometry();
-
-
-
 window.eqfeed_callback = function(results) {
 	size = results.features.length;
 	for (var i = 0; i < size; i++){
@@ -34,18 +28,8 @@ window.eqfeed_callback = function(results) {
 		}
 		sphereParent.position.set(0,0,0);
 	}
-	alert(count);
-	//scene.add(sphereParent);
-	var particle_system_material = new THREE.ParticleBasicMaterial({
-  		color: 0xffffff,
-  		size: 1
-	});
-	var particleSystem = new THREE.ParticleSystem(
-  		particle_system_geometry,
-    		particle_system_material
-	);
-	scene.add(particleSystem);
-
+	alert("earthquake count:"+count);
+	scene.add(sphereParent);
 }
 function rect(x,y){
 				
