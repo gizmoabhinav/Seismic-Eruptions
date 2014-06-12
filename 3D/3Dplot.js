20d6ced332f7fda7530f7339875c841ba5cca210
gizmoabhinav <gizmoabhinav@gmail.com>
2014-06-12 21:05:37 +0530

    colour according to depth

--- 3D/3Dplot.js
+++ 3D/3Dplot.js
@@ -1,8 +1,11 @@
 var rainbow = new Rainbow();
-rainbow.setSpectrum('blue', 'yellow', 'red');
-rainbow.setNumberRange(0, 500);
+rainbow.setSpectrum('#A52A2A', '#FF0000', '#800080', '#FF00FF', "#2f9898", "#266fc1", "#0000ff", "#00FFFF", "#50f950", "#FFFF00");
 var sphereParent = new THREE.Object3D();
-var circles = new Array();
+var radius = new Array();
+var latVal = new Array();
+var lonVal = new Array();
+var colors = new Array();
+var depths = new Array();
 var d = new Date();
 var curr_year = d.getFullYear();
 var curr_month = d.getMonth()+1;
@@ -15,22 +18,31 @@ var script = document.createElement('script');
 script.src = 'http://comcat.cr.usgs.gov/fdsnws/event/1/query?starttime='+year+'-'+month+'-'+date+'%2000:00:00&minmagnitude='+magnitude+'&format=geojson&callback=eqfeed_callback&endtime='+curr_year+'-'+curr_month+'-'+curr_date+'%2023:59:59&orderby=time-asc&minlatitude='+Math.min(getURLParameter("y1"),getURLParameter("y2"),getURLParameter("y3"),getURLParameter("y4"))+'&maxlatitude='+Math.max(getURLParameter("y1"),getURLParameter("y2"),getURLParameter("y3"),getURLParameter("y4"))+'&minlongitude='+Math.min(getURLParameter("x1"),getURLParameter("x2"),getURLParameter("x3"),getURLParameter("x4"))+'&maxlongitude='+Math.max(getURLParameter("x1"),getURLParameter("x2"),getURLParameter("x3"),getURLParameter("x4"));
 document.getElementsByTagName('body')[0].appendChild(script);
 var count = 0;
+var max = 0;
+var size;
 window.eqfeed_callback = function(results) {
 	size = results.features.length;
 	for (var i = 0; i < size; i++){
 		if(rect(convertCoordinatesx(results.features[i].geometry.coordinates[0]),convertCoordinatesy(results.features[i].geometry.coordinates[1]))){
 			count++;
-			//circles[i] = new Array(results.features[i].geometry.coordinates[0],results.features[i].geometry.coordinates[1],results.features[i].geometry.coordinates[2],results.features[i].properties.time,results.features[i].properties.mag);
-			var radius = 0.005*(results.features[i].properties.mag-magnitude+1);
-			var sphereGeometry = new THREE.SphereGeometry( radius, 4, 2 );
-			var sphereMaterial = new THREE.MeshLambertMaterial( { color: parseInt('0x'+rainbow.colourAt(results.features[i].geometry.coordinates[2])) } );
-			var sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
-			sphere.position.set(convertCoordinatesx(results.features[i].geometry.coordinates[0])-leftTileLimit-2,0.5-(results.features[i].geometry.coordinates[2]/1000),convertCoordinatesy(results.features[i].geometry.coordinates[1])-topTileLimit-2);
-			sphereParent.add( sphere );
+			if(results.features[i].geometry.coordinates[2]>max)max=results.features[i].geometry.coordinates[2];
+			radius[i] = 0.005*(results.features[i].properties.mag-magnitude+1);
+			latVal[i] = results.features[i].geometry.coordinates[0];
+			lonVal[i] = results.features[i].geometry.coordinates[1];
+			depths[i] = results.features[i].geometry.coordinates[2];
+			colors[i] = '0x'+rainbow.colourAt(results.features[i].geometry.coordinates[2]);
 		}
-		sphereParent.position.set(0,0,0);
 	}
 	alert("earthquake count : "+count);
+	rainbow.setNumberRange(0, max);
+	for(var i=0;i<size;i++){
+		var sphereGeometry = new THREE.SphereGeometry( radius[i], 4, 2 );
+		var sphereMaterial = new THREE.MeshLambertMaterial( { color: parseInt(colors[i]) } );
+		var sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
+		sphere.position.set(convertCoordinatesx(latVal[i])-leftTileLimit-2,0.5-(depths[i]/1000),convertCoordinatesy(lonVal[i])-topTileLimit-2);
+		sphereParent.add( sphere );
+	}
+	sphereParent.position.set(0,0,0);
 	scene.add(sphereParent);
 }
 function rect(x,y){
