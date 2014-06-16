ed294a87506afebdf3bf8246418ea5c2451e1f5e
gizmoabhinav <gizmoabhinav@gmail.com>
2014-06-17 00:19:09 +0530

    webgl

--- 3D/mapbox.js
+++ 3D/mapbox.js
@@ -5,8 +5,8 @@ initializeScene();
 
 animateScene();
 function initializeScene(){ 
-	if(false){ 
-		renderer = new THREE.WebGLRenderer({antialias:true}); 
+	if(Detector.webgl){ 
+		renderer = new THREE.WebGLRenderer({antialias:true});
 	} else { 
         renderer = new THREE.CanvasRenderer(); 
     } 
@@ -29,50 +29,38 @@ function initializeScene(){
 	
 	
 	controls = new THREE.OrbitControls( camera, renderer.domElement );
-	var cubeGeometry = new THREE.CubeGeometry(1, 1, 1, 2,0,2);
-	/*if(!Detector.webgl){
-		cubeGeometry = new THREE.CubeGeometry(0.5, 0.5,0.5, 4, 4, 4); 
-	}*/
+	var cubeGeometry;
+	if(Detector.webgl){
+		planeGeometry = new THREE.PlaneGeometry( 1, 1, 1); 
+	}
+	else{
+		planeGeometry = new THREE.PlaneGeometry(1, 1, 1, 2,0,2);
+	}
+	var tileSource;
+	if(Detector.webgl){
+		tileSource = "tiles/6/"; 
+	}
+	else{
+		tileSource = "http://otile1.mqcdn.com/tiles/1.0.0/sat/6/"; 
+	}
 	for(var j=0;j<4;j++){
 		for(var i=0;i<4;i++){
-			glassTexture = new THREE.ImageUtils.loadTexture("http://otile1.mqcdn.com/tiles/1.0.0/sat/6/"+(leftTileLimit+i)+"/"+(topTileLimit+j)+".png");
-			//glassTexture = new THREE.ImageUtils.loadTexture("tiles/"+(21+i)+"/"+(11+j)+".jpg");
+			
+			glassTexture = new THREE.ImageUtils.loadTexture(tileSource+(leftTileLimit+i)+"/"+(topTileLimit+j)+".png");
 			glassTexture.wrapS = glassTexture.wrapT = THREE.RepeatWrapping;
 			glassTexture.repeat.set( 1, 1 );
-			var cubeMaterials = [
-				new THREE.MeshBasicMaterial({color:0xFFFF00,transparent: true,opacity:0.01}),
-				new THREE.MeshBasicMaterial({color:0x0000FF,transparent: true,opacity:0.01}),
-				new THREE.MeshLambertMaterial({
-					map:glassTexture,
+			var planeMaterial = new THREE.MeshBasicMaterial({map:glassTexture,
 					depthWrite: false,
+					depthTest: false,
 					transparent: true,
 					opacity: 0.5,
 					side:THREE.DoubleSide,
-					combine: THREE.MixOperation
-				}),
-				new THREE.MeshBasicMaterial({color:0x00FF00,transparent: true,opacity:0.3,side:THREE.DoubleSide}),
-				new THREE.MeshBasicMaterial({color:0x00FFFF,transparent: true,opacity:0.01}),
-				new THREE.MeshBasicMaterial({color:0xFFFFFF,transparent: true,opacity:0.01})
-			];
-			var cubeMaterial = new THREE.MeshFaceMaterial(cubeMaterials);
-			cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
-			cubeMesh.position.set(i+0.5-2, 0.0, j+0.5-2);
+					combine: THREE.MixOperation});
+			cubeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+			cubeMesh.position.set(i+0.5-2,-j-0.5+2,1.0);
 			scene.add(cubeMesh);
 		}
 	}
-	// generate the rectangle
-	var rectangle = new THREE.Geometry();
-	rectangle.vertices.push( new THREE.Vector3( x1-leftTileLimit-2, 0.55, y1-topTileLimit-2 ) );
-	rectangle.vertices.push( new THREE.Vector3( x2-leftTileLimit-2, 0.55, y2-topTileLimit-2 ) );
-	rectangle.vertices.push( new THREE.Vector3( x3-leftTileLimit-2, 0.55, y3-topTileLimit-2 ) );
-	rectangle.vertices.push( new THREE.Vector3( x4-leftTileLimit-2, 0.55, y4-topTileLimit-2 ) );
-	rectangle.faces.push( new THREE.Face3( 2,1,0 ) );
-	rectangle.faces.push( new THREE.Face3( 0,3,2 ) );
-	rectangle.faces.push( new THREE.Face3( 0,1,2 ) );
-	rectangle.faces.push( new THREE.Face3( 2,3,0 ) );
-	var rectmaterial = new THREE.MeshBasicMaterial({color: 0x770000,opacity:0.3});
-	rectmesh = new THREE.Mesh(rectangle,rectmaterial);
-	scene.add(rectmesh);
 	
 	
 	var ambientLight = new THREE.AmbientLight(0x101010, 10.0);
