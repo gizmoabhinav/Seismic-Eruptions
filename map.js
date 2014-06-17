bfca1b61ba7be2d3b577eaeb41a318ac1cc2b024
gizmoabhinav <gizmoabhinav@gmail.com>
2014-06-17 20:46:26 +0530

    url parameters for 2d map

--- map.js
+++ map.js
@@ -1,3 +1,23 @@
+//get url params
+var mag = getURLParameter("mag");
+if(mag == undefined){
+	mag = 5;
+}
+var startyear = getURLParameter("startyear");
+if(startyear == undefined){
+	startyear = 2009;
+}
+var startmonth = getURLParameter("startmonth");
+if(startmonth == undefined){
+	startmonth = 1;
+}
+var startdate = getURLParameter("startdate");
+if(startdate == undefined){
+	startdate = 1;
+}
+var endyear = getURLParameter("endyear");
+var endmonth = getURLParameter("endmonth");
+var enddate = getURLParameter("enddate");
 //load map
 var speed = 6;
 var map = L.map('map').setView([30, -0], 2);
@@ -14,6 +34,15 @@ var curr_year = d.getFullYear();
 var curr_month = d.getMonth()+1;
 var curr_date = d.getDate();
 
+if(endyear == undefined){
+	endyear = curr_year;
+}
+if(endmonth == undefined){
+	endmonth = curr_month;
+}
+if(enddate == undefined){
+	enddate = curr_date;
+}
 //time stamp conversion
 function timeConverter(UNIX_timestamp){
 	var a = new Date(UNIX_timestamp);
@@ -41,8 +70,7 @@ var circles_added = new MiniSet();
 var script = document.createElement('script');
 var snd = new Audio("tap.wav"); // buffers automatically when created
 var rainbow = new Rainbow();
-
-script.src = 'http://comcat.cr.usgs.gov/fdsnws/event/1/query?starttime=2013-1-1%2000:00:00&minmagnitude=5&format=geojson&callback=eqfeed_callback&endtime='+curr_year+'-'+curr_month+'-'+curr_date+'%2023:59:59&orderby=time-asc';
+script.src = 'http://comcat.cr.usgs.gov/fdsnws/event/1/query?starttime='+startyear+'-'+startmonth+'-'+startdate+'%2000:00:00&minmagnitude='+mag+'&format=geojson&callback=eqfeed_callback&endtime='+endyear+'-'+endmonth+'-'+enddate+'%2023:59:59&orderby=time-asc';
 document.getElementsByTagName('body')[0].appendChild(script);
 window.eqfeed_callback = function(results) {
 	  
