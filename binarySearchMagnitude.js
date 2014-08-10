144470579a6bf8b0850fa78e558ff9c3ca92a31b
gizmoabhinav <gizmoabhinav@gmail.com>
2014-08-10 11:41:42 +0530

    bug fix

--- binarySearchMagnitude.js
+++ binarySearchMagnitude.js
@@ -11,7 +11,7 @@ function loadCount(click){
 		$("#error-date").html("");
 	}
 	if(click==0)
-	window.open("http://gizmoabhinav.github.io/Seismic-Eruptions/?mag="+binarySearch(0,100)+"&startdate="+(parseInt(year1)+1900)+"-"+(parseInt(month1)+1)+"-1&enddate="+(parseInt(year2)+1900)+"-"+(parseInt(month2)+1)+"-1","_self");
+	window.open("http://gizmoabhinav.github.io/Seismic-Eruptions/?mag="+binarySearch(0,100)+"&startdate="+(parseInt(year1)+1960)+"-"+(parseInt(month1)+1)+"-1&enddate="+(parseInt(year2)+1960)+"-"+(parseInt(month2)+1)+"-1","_self");
 	else
 	$("#magnitude-search").html("<p>Calculated magnitude cutoff : </p><p style='color:green'>"+binarySearch(0,100)+"</p>");
 };
