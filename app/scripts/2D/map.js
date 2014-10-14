1d6bcc563744446fda455895afdc976358027e4d
gizmoabhinav <gizmoabhinav@gmail.com>
2014-10-14 20:52:20 +0530

    tidy up

--- app/scripts/2D/map.js
+++ app/scripts/2D/map.js
@@ -25,10 +25,10 @@ var map2D = (function () {
         },
 
         values: {
-            timediff: 0,
-            size: 0,
-            maxdepth: 0,
-            mindepth: 2000,
+            timediff: 0, //the total time between the first event and the last
+            size: 0, //number of earthquakes
+            maxdepth: 0, //maximum depth of an earthquake
+            mindepth: 2000 //minimum depth of an earthquake
         },
 
         layers: {
@@ -37,22 +37,24 @@ var map2D = (function () {
             baseLayer1: L.tileLayer('http://{s}.tiles.mapbox.com/v3/bclc-apec.map-rslgvy56/{z}/{x}/{y}.png', {})
         },
 
-        drawnItems: new L.FeatureGroup(),
+        drawnItems: new L.FeatureGroup(), //features drawn on the map (constitute the cross-section)
 
-        circles: new Array(),
-        time: new Array(),
-        stamp: new Array(),
-        depth: new Array(),
+        earthquakes: {
+            circles: new Array(), // Array of earthquake markers
+            time: new Array(), // time of occurrence of corresponding earthquakes
+            depth: new Array() // Array of depths of corresponding earthquakes
+        },
 
         array: new Array(),
         magarray: {},
 
-        editing: false,
+        editing: false, //state of the map
 
-        plateBoundaries: new L.KML("../../assets/2D/plates.kml", {
+        plateBoundaries: new L.KML("../../assets/2D/plates.kml", { // KML containing plate boundary
             async: true
         }),
 
+        // toggle plate boundaries
         plateToggle: function () {
             if ($("#plates").is(':checked')) {
                 this.leafletMap.addLayer(this.plateBoundaries); // checked
@@ -61,112 +63,126 @@ var map2D = (function () {
             }
         },
 
+        //	add earthquake event
         mapAdder: function (i) {
-            if (!map.leafletMap.hasLayer(map.circles[i])) {
-                map.circles[i].addTo(map.leafletMap);
+            if (!map.leafletMap.hasLayer(map.earthquakes.circles[i])) {
+                map.earthquakes.circles[i].addTo(map.leafletMap);
             }
-            map.circles[i].setStyle({
+            map.earthquakes.circles[i].setStyle({
                 fillOpacity: 0.5,
-                fillColor: "#" + rainbow.colourAt(map.depth[i])
+                fillColor: "#" + rainbow.colourAt(map.earthquakes.depth[i])
             });
             i++;
-            while (map.leafletMap.hasLayer(map.circles[i])) {
-                map.leafletMap.removeLayer(map.circles[i]);
+            while (map.leafletMap.hasLayer(map.earthquakes.circles[i])) {
+                map.leafletMap.removeLayer(map.earthquakes.circles[i]);
                 i++;
             }
-            $("#time").html(map.time[i]);
+            $("#time").html(timeConverter(map.earthquakes.time[i]));
             controller.snd.play();
         },
 
+        // remove earthquake event
         mapRemover: function (i) {
-            if (map.leafletMap.hasLayer(map.circles[i])) {
-                map.leafletMap.removeLayer(map.circles[i]);
+            if (map.leafletMap.hasLayer(map.earthquakes.circles[i])) {
+                map.leafletMap.removeLayer(map.earthquakes.circles[i]);
             }
         },
 		
-		render:function () {
-        if (this.editing) {
-            alert("Save edit before viewing the cross section");
-            return;
-        }
-        if (linelength == 0) {
-            alert("Draw a cross-section first");
-            return;
-        } else if (linelength >= 1400) {
-            alert("cross section too long");
-            return;
-        }
-        window.open("../3D/index.html?x1=" + toLon(y1) + "&y1=" + toLat(x1) + "&x2=" + toLon(y2) + "&y2=" + toLat(x2) + "&x3=" + toLon(y3) + "&y3=" + toLat(x3) + "&x4=" + toLon(y4) + "&y4=" + toLat(x4) + "&mag=" + map.parameters.mag + "&startdate=" + map.parameters.startdate + "&enddate=" + map.parameters.enddate);
-    },
-    poly:{}, polyedit:{},
-
-    startdrawing: function () {
-        if (this.editing) {
-            alert("Save edit before drawing a new cross-section");
-            return;
-        }
-        this.poly = new L.Draw.CrossSection(map.leafletMap, []);
-        this.poly.enable();
-        this.poly._updateTooltip();
-    },
-
-    editdrawing:function () {
-        this.editing = true;
-        this.polyedit = new L.EditToolbar.Edit(map, {
-            featureGroup: this.drawnItems,
-            selectedPathOptions: {
-                color: '#fe57a1',
-                opacity: 0.6,
-                dashArray: '10, 10',
-                fill: true,
-                fillColor: '#fe57a1',
-                fillOpacity: 0.1,
-                maintainColor: false
+		// render the cross section
+        render: function () {
+            if (this.editing) {
+                alert("Save edit before viewing the cross section");
+                return;
             }
-        });
-        this.polyedit.enable();
-    },
+            if (linelength == 0) {
+                alert("Draw a cross-section first");
+                return;
+            } else if (linelength >= 1400) {
+                alert("cross section too long");
+                return;
+            }
+            window.open("../3D/index.html?x1=" + toLon(y1) + "&y1=" + toLat(x1) + "&x2=" + toLon(y2) + "&y2=" + toLat(x2) + "&x3=" + toLon(y3) + "&y3=" + toLat(x3) + "&x4=" + toLon(y4) + "&y4=" + toLat(x4) + "&mag=" + map.parameters.mag + "&startdate=" + map.parameters.startdate + "&enddate=" + map.parameters.enddate);
+        },
+		
+        poly: {},
+        polyedit: {},
+		
+		// Start a new cross section drawing
+        startdrawing: function () {
+            if (this.editing) {
+                alert("Save edit before drawing a new cross-section");
+                return;
+            }
+            this.poly = new L.Draw.CrossSection(map.leafletMap, []);
+            this.poly.enable();
+            this.poly._updateTooltip();
+        },
 
-    editsave:function () {
-        this.editing = false;
-        this.polyedit.save();
-        this.polyedit.disable();
-        this.polyedit = null;
-    },
+		//Edit the cross section drawing
+        editdrawing: function () {
+            this.editing = true;
+            this.polyedit = new L.EditToolbar.Edit(map, {
+                featureGroup: this.drawnItems,
+                selectedPathOptions: {
+                    color: '#fe57a1',
+                    opacity: 0.6,
+                    dashArray: '10, 10',
+                    fill: true,
+                    fillColor: '#fe57a1',
+                    fillOpacity: 0.1,
+                    maintainColor: false
+                }
+            });
+            this.polyedit.enable();
+        },
 
-    backtonormalview:function () {
-        this.editing = false;
-        if (this.polyedit != null) editsave();
-        this.poly.disable();
-        this.poly = null;
-    }
+		//save the edit
+        editsave: function () {
+            this.editing = false;
+            this.polyedit.save();
+            this.polyedit.disable();
+            this.polyedit = null;
+        },
+
+		//go back to playback
+        backtonormalview: function () {
+            this.editing = false;
+            if (this.polyedit != null) editsave();
+            this.poly.disable();
+            this.poly = null;
+        }
 
     };
+	
+	//	colour gradient generator
+    var rainbow = new Rainbow();
 
     var controller = {
+	
+		// timeline of events
         timeLine: new TimelineLite({
             onUpdate: updateSlider
         }),
 
+		//speed of events
         speed: 6,
 
-
-
-
+		// output of usgs query
         script: document.createElement('script'),
 
-        snd: new Audio("../../assets/2D/tap.wav"), // buffers automatically when created
+        // sound of the audio
+		snd: new Audio("../../assets/2D/tap.wav"), // buffers automatically when created
 
-        initController: function () {
+		initController: function () {
 
-            this.script.src = 'http://comcat.cr.usgs.gov/fdsnws/event/1/query?starttime=' + map.parameters.startdate + '%0000:00:00&minmagnitude=' + map.parameters.mag + '&format=geojson&callback=eqfeed_callback&endtime=' + map.parameters.enddate + '%0000:00:00&orderby=time-asc';
+			this.script.src = 'http://comcat.cr.usgs.gov/fdsnws/event/1/query?starttime=' + map.parameters.startdate + '%0000:00:00&minmagnitude=' + map.parameters.mag + '&format=geojson&callback=eqfeed_callback&endtime=' + map.parameters.enddate + '%0000:00:00&orderby=time-asc';
             document.getElementsByTagName('body')[0].appendChild(this.script);
             window.eqfeed_callback = function (results) {
 
                 map.values.size = results.features.length;
 
                 for (var i = 0; i < map.values.size; i++) {
-                    map.circles[i] = L.geoJson(results.features[i], {
+                    map.earthquakes.circles[i] = L.geoJson(results.features[i], {
                         pointToLayer: function (feature, latlng) {
                             return L.circleMarker(latlng, {
                                 radius: results.features[i].properties.mag,
@@ -178,19 +194,20 @@ var map2D = (function () {
                             });
                         }
                     }).bindPopup("Place: <b>" + results.features[i].properties.place + "</b></br>Magnitude : <b>" + results.features[i].properties.mag + "</b></br>Time : " + timeConverter(results.features[i].properties.time) + "</br>Depth : " + results.features[i].geometry.coordinates[2] + " km");
-                    //circles[i].addTo(map.leafletMap);
-                    map.time[i] = timeConverter(results.features[i].properties.time);
-                    map.stamp[i] = results.features[i].properties.time
-                    map.depth[i] = results.features[i].geometry.coordinates[2];
-                    if (map.depth[i] > map.values.maxdepth) map.values.maxdepth = map.depth[i];
-                    if (map.depth[i] < map.values.mindepth) map.values.mindepth = map.depth[i];
-                    // Adding events to the timeline
-                    //if(i==0)
-                    //tl.append(TweenLite.delayedCall(0,mapAdder,[i.toString]));
-                    if (i > 0) {
-                        controller.timeLine.append(TweenLite.delayedCall(20 * ((results.features[i].properties.time - results.features[i - 1].properties.time) / 1000000000), map.mapAdder, [i.toString()]));
-                    }
+                    
+                    map.earthquakes.time[i] = results.features[i].properties.time
+                    map.earthquakes.depth[i] = results.features[i].geometry.coordinates[2];
+                    if (map.earthquakes.depth[i] > map.values.maxdepth) map.values.maxdepth = map.earthquakes.depth[i];
+                    if (map.earthquakes.depth[i] < map.values.mindepth) map.values.mindepth = map.earthquakes.depth[i];
+                    
+					// add events to timeline
+					if(i>0){
+						controller.timeLine.append(TweenLite.delayedCall(20 * ((results.features[i].properties.time - results.features[i - 1].properties.time) / 1000000000), map.mapAdder, [i.toString()]));
+					}else{
+						controller.timeLine.append(TweenLite.delayedCall(0, map.mapAdder, [i.toString()]));
+					}
                 }
+				
                 rainbow.setNumberRange(map.values.mindepth, map.values.maxdepth);
                 map.values.timediff = results.features[map.values.size - 1].properties.time - results.features[0].properties.time;
                 map.parameters.starttime = results.features[0].properties.time;
@@ -202,16 +219,21 @@ var map2D = (function () {
                     max: map.values.timediff,
                     slide: function (event, ui) {
                         $("#date").html(timeConverter(map.parameters.starttime));
-                        //tl.seek();
                         controller.timeLine.pause();
                         controller.timeLine.progress(ui.value / (map.values.timediff));
                     }
                 })
+				
+				$("#info").html("</br></br>total earthquakes : " + map.values.size + "</br>minimum depth : " + map.values.mindepth + " km</br>maximum depth : " + map.values.maxdepth + " km</br></br></br><div class='ui-body ui-body-a'><p><a href='http://github.com/gizmoabhinav/Seismic-Eruptions'>Link to the project</a></p></div>");
+				$("#startdate").html("Start date : " + timeConverter(map.parameters.startdate));
+				$("#enddate").html("End date : " + timeConverter(map.parameters.enddate));
+				$("#magcutoff").html("Cutoff magnitude : " + map.parameters.mag);
             }
+			loadCountFile();
+			
         }
     };
 
-    var rainbow = new Rainbow();
 
     function getURLParameter(name) {
         return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [, ""])[1].replace(/\+/g, '%20')) || null
@@ -232,7 +254,7 @@ var map2D = (function () {
     }
 
     //load count file
-    var loadCountFile = (function () {
+    function loadCountFile() {
         $.get('count.txt', function (data) {
             array = data.split(',');
             console.log(array);
@@ -250,7 +272,7 @@ var map2D = (function () {
             }
             console.log(magarray);
         });
-    });
+    }
 
     function updateSlider() {
         $("#slider").slider("value", (controller.timeLine.progress() * map.values.timediff));
@@ -280,10 +302,7 @@ var map2D = (function () {
         controller.timeLine.pause();
         controller.initController();
 
-        $("#info").html("</br></br>total earthquakes : " + controller.size + "</br>minimum depth : " + controller.mindepth + " km</br>maximum depth : " + controller.maxdepth + " km</br></br></br><div class='ui-body ui-body-a'><p><a href='http://github.com/gizmoabhinav/Seismic-Eruptions'>Link to the project</a></p></div>");
-        $("#startdate").html("Start date : " + timeConverter(map.parameters.startdate));
-        $("#enddate").html("End date : " + timeConverter(map.parameters.enddate));
-        $("#magcutoff").html("Cutoff magnitude : " + map.parameters.mag);
+        
 
         $.mobile.loading('hide');
         setTimeout(function () {
@@ -381,12 +400,12 @@ var map2D = (function () {
             $('#playback').fadeOut();
             $('#crosssection').fadeIn();
             for (var i = 0; i < map.values.size; i++) {
-                if (!map.leafletMap.hasLayer(map.circles[i])) {
-                    map.circles[i].setStyle({
+                if (!map.leafletMap.hasLayer(map.earthquakes.circles[i])) {
+                    map.earthquakes.circles[i].setStyle({
                         fillOpacity: 0.5,
-                        fillColor: "#" + rainbow.colourAt(map.depth[i])
+                        fillColor: "#" + rainbow.colourAt(map.earthquakes.depth[i])
                     });
-                    map.circles[i].addTo(map.leafletMap);
+                    map.earthquakes.circles[i].addTo(map.leafletMap);
                 }
             }
             $.mobile.loading('hide');
@@ -463,6 +482,6 @@ var map2D = (function () {
     $('#date-2-m').change(function () {
         loadCount(1);
     });
-	
+
     return map;
 })();
